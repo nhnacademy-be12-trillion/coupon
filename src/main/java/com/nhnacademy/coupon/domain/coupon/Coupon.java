5deb0c13d23c99a9f88de6a2a93fef74cd54f8eb
hyunsu15 <wjdgustn15@naver.com>
2025-11-14 15:49:25 +0900

    test: coupon isActive 추가 &&coupon test추가

--- src/main/java/com/nhnacademy/coupon/domain/coupon/Coupon.java
+++ src/main/java/com/nhnacademy/coupon/domain/coupon/Coupon.java
@@ -1,11 +1,10 @@
 package com.nhnacademy.coupon.domain.coupon;
 
+import com.nhnacademy.coupon.error.CustomException;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class Coupon {
     private Long id;
     private String name;
@@ -13,7 +12,26 @@ public class Coupon {
     private Long quantity;
     private LocalDateTime issueStartDate;
     private LocalDateTime issueEndDate;
+
+    public Coupon(Long id, String name, Long policyId, Long quantity, LocalDateTime issueStartDate,
+                  LocalDateTime issueEndDate) {
+        if(issueEndDate.isBefore(issueStartDate)||issueEndDate.isEqual(issueStartDate)) {
+            throw new CustomException("error.message.illegalDate",new Object[]{issueStartDate,issueEndDate});
+        }
+        this.id = id;
+        this.name = name;
+        this.policyId = policyId;
+        this.quantity = quantity;
+        this.issueStartDate = issueStartDate;
+        this.issueEndDate = issueEndDate;
+    }
+
     public CouponType getType(){
         return CouponType.DEFAULT;
+    }
+    public boolean isActive(){
+        if(quantity==null)
+            return true;
+        return quantity>0;
     }
 }
---
+++ src/test/java/com/nhnacademy/coupon/domain/coupon/CouponTest.java
@@ -0,0 +1,78 @@
+package com.nhnacademy.coupon.domain.coupon;
+
+import com.nhnacademy.coupon.error.CustomException;
+import java.time.LocalDateTime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CouponTest {
+    @Test
+    @DisplayName("끝나는 시간이 시작시간보다 앞이면 예외반환")
+    void testCoupon() {
+        LocalDateTime start = LocalDateTime.now();
+        LocalDateTime end= start.minusSeconds(1);
+
+        Assertions.assertThatThrownBy(()->new Coupon(null,"qwe",1L,null,start,end)).isInstanceOf(CustomException.class);
+    }
+    @Test
+    @DisplayName("끝나는 시간이 시작시간이 같으면 예외반환")
+    void testCoupon1() {
+        LocalDateTime start = LocalDateTime.now();
+        LocalDateTime end= start;
+
+        Assertions.assertThatCode(()->new Coupon(null,"qwe",1L,null,start,end)).isInstanceOf(CustomException.class);
+    }
+
+    @Test
+    @DisplayName("정상적으로 생성된다.")
+    void testCoupon2() {
+        LocalDateTime start = LocalDateTime.now();
+        LocalDateTime end= start.plusSeconds(1);
+
+        Assertions.assertThatCode(()->new Coupon(null,"qwe",1L,null,start,end)).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("쿠폰퀄리티가 null이면 활성화된다.")
+    void actviceTest(){
+        LocalDateTime start = LocalDateTime.now();
+        LocalDateTime end= start.plusSeconds(1);
+        boolean except = true;
+        Coupon coupon = new Coupon(null, "qwe", 1L, null, start, end);
+        Assertions.assertThat(coupon.isActive()).isEqualTo(except);
+    }
+    @Test
+    @DisplayName("쿠폰퀄리티가 null이면 활성화된다.")
+    void actviceTest2(){
+        LocalDateTime start = LocalDateTime.now();
+        LocalDateTime end= start.plusSeconds(1);
+        boolean except = true;
+        Coupon coupon = new Coupon(null, "qwe", 1L, null, start, end);
+        Assertions.assertThat(coupon.isActive()).isEqualTo(except);
+    }
+    @ParameterizedTest
+    @ValueSource(longs = {1,Integer.MAX_VALUE,Long.MAX_VALUE})
+    @DisplayName("쿠폰퀄리티가 양수면 활성화가 된다.")
+    void actviceTest4(long value){
+        LocalDateTime start = LocalDateTime.now();
+        LocalDateTime end= start.plusSeconds(1);
+        boolean except = true;
+        Coupon coupon = new Coupon(null, "qwe", 1L, value, start, end);
+        Assertions.assertThat(coupon.isActive()).isEqualTo(except);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(longs = {-1,0})
+    @DisplayName("쿠폰퀄리티가 양수가 아니면 활성화가 안된다.")
+    void actviceTest1(long value){
+        LocalDateTime start = LocalDateTime.now();
+        LocalDateTime end= start.plusSeconds(1);
+        boolean except = false;
+        Coupon coupon = new Coupon(null, "qwe", 1L, value, start, end);
+        Assertions.assertThat(coupon.isActive()).isEqualTo(except);
+    }
+}
